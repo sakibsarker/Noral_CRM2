c2bf0edad2f105767c9e7e4eca9ebf1333ce822d
sakibsarker <sakib15-3824@diu.edu.bd>
2023-08-28 20:31:19 +0600

    vecel

--- next.config.js
+++ next.config.js
@@ -1,81 +1,75 @@
-// /**
-//  * @type {import('next').NextConfig}
-//  **/
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   async redirects() {
-//     return [
-//       {
-//         source: '/apps',
-//         destination: '/apps/invoicing/invoice',
-//         permanent: true,
-//       },
-//       {
-//         source: '/apps/invoicing',
-//         destination: '/apps/invoicing/invoice',
-//         permanent: true,
-//       },
-//       {
-//         source: '/components',
-//         destination: '/components/base/accordion',
-//         permanent: true,
-//       },
-//       {
-//         source: '/components/base',
-//         destination: '/components/base/accordion',
-//         permanent: true,
-//       },
-//       {
-//         source: '/components/buttons',
-//         destination: '/components/buttons/buttons',
-//         permanent: true,
-//       },
-//       {
-//         source: '/components/forms',
-//         destination: '/components/forms/form-control',
-//         permanent: true,
-//       },
-//       {
-//         source: '/components/icons',
-//         destination: '/components/icons/free',
-//         permanent: true,
-//       },
-//       {
-//         source: '/components/notifications',
-//         destination: '/components/notifications/alerts',
-//         permanent: true,
-//       },
-//       {
-//         source: '/plugins',
-//         destination: '/plugins/calendar',
-//         permanent: true,
-//       },
-//     ]
-//   },
-//   async headers() {
-//     return [
-//       {
-//         // matching all API routes
-//         source: "/api/:path*",
-//         headers: [
-//           { key: "Access-Control-Allow-Credentials", value: "true" },
-//           { key: "Access-Control-Allow-Origin", value: "*" },
-//           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//         ]
-//       }
-//     ]
-//   }
-// }
-
-// module.exports = nextConfig
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/apps',
+        destination: '/apps/invoicing/invoice',
+        permanent: true,
+      },
+      {
+        source: '/apps/invoicing',
+        destination: '/apps/invoicing/invoice',
+        permanent: true,
+      },
+      {
+        source: '/components',
+        destination: '/components/base/accordion',
+        permanent: true,
+      },
+      {
+        source: '/components/base',
+        destination: '/components/base/accordion',
+        permanent: true,
+      },
+      {
+        source: '/components/buttons',
+        destination: '/components/buttons/buttons',
+        permanent: true,
+      },
+      {
+        source: '/components/forms',
+        destination: '/components/forms/form-control',
+        permanent: true,
+      },
+      {
+        source: '/components/icons',
+        destination: '/components/icons/free',
+        permanent: true,
+      },
+      {
+        source: '/components/notifications',
+        destination: '/components/notifications/alerts',
+        permanent: true,
+      },
+      {
+        source: '/plugins',
+        destination: '/plugins/calendar',
+        permanent: true,
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/pages/Employees/TableEmployees.tsx
+++ src/pages/Employees/TableEmployees.tsx
@@ -25,12 +25,12 @@ type Props = {
   apiRoute: String[];
 };
 
-
 const updateEmployee = async (item: any, updatedData: any) => {
   try {
-    const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/employees/updateEmployee`;
+    
+    const apiUrl =`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/employees/updateEmployee`;
     const response = await fetch(apiUrl, {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
@@ -174,7 +174,6 @@ function EmployeeTable(props: Props) {
       itemsPerPage={10}
       columnSorter
       pagination
-     
       scopedColumns={{
         avatar: (item: any) => {
           return (
